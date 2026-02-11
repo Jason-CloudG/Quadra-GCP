436d7ac5190d66f02f2d4a65edcab1c4bd83f049
Jason-CloudG <135985363+Jason-CloudG@users.noreply.github.com>
2026-02-11 13:15:59 +0530

    Update main.tf

--- staging/main.tf
+++ staging/main.tf
@@ -1,1 +1,40 @@
+resource "google_compute_network" "vpc" {
+  name                    = "tf-vpc"
+  auto_create_subnetworks = false
+}
 
+resource "google_compute_subnetwork" "subnet" {
+  name          = "tf-subnet"
+  ip_cidr_range = "10.10.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+resource "google_compute_firewall" "ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "tf-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.id
+
+    access_config {}
+  }
+}
