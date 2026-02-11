baab3452bdf87ad566244ad0144407068ce2adb4
Jason-CloudG <135985363+Jason-CloudG@users.noreply.github.com>
2026-02-11 13:35:56 +0530

    Update variables.tf

--- staging/variables.tf
+++ staging/variables.tf
@@ -1,5 +1,12 @@
+variable "GOOGLE_CREDENTIALS" {
+  description = "The credential file for GCP"
+  type        = string
+  sensitive   = true
+}
+
 variable "project_id" {
   type = string
+  default = "venkatesh-sandbox-new"
 }
 
 variable "region" {
