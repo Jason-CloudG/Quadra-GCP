a67b8c93287dc7af673e198f38f7111bb37bd74f
Jason-CloudG <135985363+Jason-CloudG@users.noreply.github.com>
2026-02-20 16:52:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+resource "google_compute_network" "custom_vpc" {
+  name                    = "custom-vpc-tf"
+  auto_create_subnetworks = false
+  routing_mode            = "GLOBAL"
+  project                 = var.project_id
+}
 
+resource "google_compute_subnetwork" "custom_subnet" {
+  name          = "custom-subnet-tf"
+  ip_cidr_range = "10.10.0.0/24"
+  region        = var.region
+  network       = google_compute_network.custom_vpc.id
+  project       = var.project_id
+}
